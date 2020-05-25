104dd19edb54cb4fa41a04a6b046457537b5cf91
Alex Baranowski <alex@euro-linux.com>
2020-05-25 23:11:06 +0200

    Working version - finished project 5

---
+++ project-5-vagrant-version-check-qa/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./vagrant_version_check.py  -p libvirt -p virtualbox eurolinux-vagrant/centos-6 eurolinux-vagrant/centos-7 eurolinux-vagrant/centos-8 eurolinux-vagrant/oracle-linux-6 eurolinux-vagrant/oracle-linux-7  eurolinux-vagrant/oracle-linux-8 eurolinux-vagrant/scientific-linux-6 eurolinux-vagrant/scientific-linux-7 eurolinux-vagrant/eurolinux-6 eurolinux-vagrant/eurolinux-7
--- project-5-vagrant-version-check-qa/vagrant_version_check.py
+++ project-5-vagrant-version-check-qa/vagrant_version_check.py
@@ -7,7 +7,6 @@ import argparse
 
 def check_vagrant_box(vagrant_box, providers):
     api_url = 'https://app.vagrantup.com/api/v1/box/{}'.format(vagrant_box)
-    print("checking {} with providers {}".format(vagrant_box, providers))
     r = requests.get(api_url)
     assert r.status_code == 200
     vagrantup_dict = json.loads(r.text)
@@ -24,13 +23,13 @@ def check_vagrant_box(vagrant_box, providers):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Give me fuel")
+    parser = argparse.ArgumentParser(description="This script check if the \
+        newest vagrant box has given providers")
     parser.add_argument('-p', '--provider', help='Vagrant provider that is\
                         required', required=True, action='append')
     parser.add_argument('vagrant_boxes', help="Vagrant box/es to check",
                         nargs='+')
     args = parser.parse_args()
-    print(args)
     fail_flag = False
     failed_boxes = []
     for box in args.vagrant_boxes:
@@ -40,8 +39,8 @@ def main():
             failed_boxes.append(box)
 
     if fail_flag:
-        print("There is problem with following boxes")
-        print(' '.join(failed_boxes))
+        print("There is problem with following boxes: {}".
+              format(' '.join(failed_boxes)))
         exit(1)
 
     print("All providers for given boxes were found!")
